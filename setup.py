3d48b0d15fc56b7c369b784e83d75b5a5a5d5547
GC <66906618+hopto-dot@users.noreply.github.com>
2024-03-10 14:19:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='YomitanDic',
-    version='0.1.0',
+    version='0.1.2',
     description='A library for easily creating dictionary files importable into Yomichan for Japanese popup dictionaries',
     author='JawGBoi',
     packages=find_packages(),
-    install_requires=[
-        "json",
-        "os",
-        "zipfile",
-        "html"
-    ],
     python_requires='>=3.6',
 )
